83b9d29213c7f658b68c45f0b8d026544d185db6
younasBnHasainar <unusbnhassainar@gmail.com>
2023-08-24 23:13:39 +0530

    challenge completed

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# FAQ-accordion-card
+# Frontend Mentor - FAQ accordion card solution
 
-Build Purpose : Learning - [Front End Mentor Challege](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam)
+### Build Purpose: Learning - Challenge - [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam).
+
+### Description:
+
+FAQ Section for websites.
+
+### Features
+
+- Responsive for mobile and different devices.
+- Easily customisable
+
+## PREVIEW
+
+![Screenshot of desktop preview](images/solution_preview_desktop.png)
+
+![Screenshot of mobile preview](images/solution_preview_mobile.png)
+
+### Personal Notes
+
+- Learnt Node nextSibling property : to select sibling element of an element using DOM API.
+
+- Learnt
+  DOMTokenList contains() method.
+  We cant use includes method on DOMTokenList which is returned by Node.classList. We've to use contains()
+
+- HTMLElement: dataset property. We can give customized attributes for html elements and can be manipulated same with Node.dataset property.
+
+### Observations
+
+- Observations on Z index
+- Observations on Overflow
--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-const questionBlock = document.querySelectorAll(".question-block")
+const questionBlocks = document.querySelectorAll(".question-block")
 const answers = document.querySelectorAll(".answer")
 const arrowKeys = document.querySelectorAll(".arrow-image")
 
-for (let count = 0; count < questionBlock.length; count++) {
-  questionBlock[count].addEventListener("click", () => {
-    console.log("hey iam inside listerner")
+questionBlocks.forEach((questionBlock) => {
+  questionBlock.addEventListener("click", () => {
+    // hiding all other answers
     answers.forEach((answer, index) => {
-      if (answer.classList.contains("active") && index != count) {
+      if (
+        index != Number(questionBlock.dataset.index) &&
+        answer.classList.contains("active")
+      ) {
         answer.classList.replace("active", "inactive")
+        questionBlocks[index].style.fontWeight = "unset"
+        arrowKeys[index].style.rotate = "0deg"
       }
-      questionBlock[index].style.fontWeight = "unset"
-      arrowKeys[index].style.rotate = "0deg"
     })
-    let update = ""
-    if (answers[count].classList.contains("inactive")) {
-      console.log("in active")
-      update = "active"
-      //   answers[count].classList.replace("inactive", "active")
-    } else if (answers[count].classList.contains("active")) {
-      let update = "inactive"
-      //   answers[count].classList.replace("active", "inactive")
-    }
-    if (update == "active") {
-      answers[count].classList.replace("inactive", "active")
-      console.log("gonna active")
-    } else {
-      answers[count].classList.replace("active", "inactive")
-    }
-    questionBlock[count].style.fontWeight = "700"
-    arrowKeys[count].style.rotate = "180deg"
-  })
-}
 
-// questionBlock[1].onclick = hi
-// function hi() {
-//   console.log("hi...")
-// }
+    // showing answer for  clicked element
+    if (
+      answers[Number(questionBlock.dataset.index)].classList.contains(
+        "inactive"
+      )
+    ) {
+      answers[Number(questionBlock.dataset.index)].classList.replace(
+        "inactive",
+        "active"
+      )
+
+      questionBlock.style.fontWeight = "700"
+      arrowKeys[Number(questionBlock.dataset.index)].style.rotate = "180deg"
+    } else {
+      answers[Number(questionBlock.dataset.index)].classList.replace(
+        "active",
+        "inactive"
+      )
+      questionBlock.style.fontWeight = "unset"
+      arrowKeys[Number(questionBlock.dataset.index)].style.rotate = "0deg"
+    }
+  })
+})
